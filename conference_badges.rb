fb560001227a79df0ae33cf07a4f7e54de8a4c82
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-20 19:09:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do |name, index|
+  attendees.collect_with_index do |name, index|
   "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end
 end
